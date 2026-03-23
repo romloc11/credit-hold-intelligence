6aff508bc6256cad4901f23634c16aa9c43ff579
Román Alejandro López Ruvalcaba <romloc1706@hotmail.com>
2026-03-23 12:01:24 -0600

    Update proc_load_bridge_interlocutores.sql

--- scripts/analytics/proc_load_bridge_interlocutores.sql
+++ scripts/analytics/proc_load_bridge_interlocutores.sql
@@ -1,3 +1,10 @@
+CREATE OR ALTER PROCEDURE analytics.proc_load_bridge_cliente_empleado
+AS
+BEGIN
+
+SET NOCOUNT ON;
+
+
 WITH interlocutores_actuales AS (
 
 -- VENDEDOR
@@ -106,3 +113,54 @@ LEFT JOIN silver.odoo_hr_employee e
 WHERE p.company_type = 'COMPANY'
 
 )
+
+UPDATE b
+SET
+
+    fecha_fin = GETDATE(),
+    es_actual = 0
+
+FROM analytics.bridge_cliente_empleado b
+
+JOIN interlocutores_actuales s
+    ON b.cliente_id = s.cliente_id
+    AND b.rol = s.rol
+
+WHERE b.es_actual = 1
+AND b.empleado_id <> s.empleado_id;
+
+
+-- insertar nuevos registros
+
+INSERT INTO analytics.bridge_cliente_empleado
+(
+    cliente_id,
+    empleado_id,
+    rol,
+    fecha_inicio,
+    fecha_fin,
+    es_actual
+)
+
+SELECT
+
+    s.cliente_id,
+    s.empleado_id,
+    s.rol,
+    GETDATE(),
+    NULL,
+    1
+
+FROM interlocutores_actuales s
+
+LEFT JOIN analytics.bridge_cliente_empleado b
+    ON s.cliente_id = b.cliente_id
+    AND s.rol = b.rol
+    AND b.es_actual = 1
+
+WHERE b.cliente_id IS NULL
+OR b.empleado_id <> s.empleado_id;
+
+END
+
+
